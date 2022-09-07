99249b1afd9b8b5201ab3a34a530d800dedf1316
noelcv <79803356+noelcv@users.noreply.github.com>
2022-09-07 15:05:49 +0200

    setup: implement Controller config

--- build/router.js
+++ build/router.js
@@ -1,8 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const index_controller_1 = __importDefault(require("./controllers/index.controller"));
 const router = (0, express_1.Router)();
-router.get("/", (req, res) => {
-    res.send("Hello Whois Query Server");
-});
+router.get("/", index_controller_1.default.home.getHome);
 exports.default = router;
--- router.ts
+++ router.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import controllers from "./controllers/index.controller";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello Whois Query Server");
-});
+router.get("/", controllers.home.getHome);
 
 export default router;
