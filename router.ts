352833b9ae014be4a045b8947c0c5c3d531df5ab
noelcv <79803356+noelcv@users.noreply.github.com>
2022-09-15 23:21:26 +0200

    chore: remove dead code

--- router.ts
+++ router.ts
@@ -4,7 +4,6 @@ import controllers from "./controllers/index.controller";
 const router = Router();
 
 router.get("/", controllers.home.getHome);
-router.get("/whois", controllers.whois.lookUp)
-router.post("/whois", controllers.whois.addLog)
+router.get("/whois", controllers.whois.lookUp);
 
 export default router;
