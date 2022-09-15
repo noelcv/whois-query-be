ef1ca29526b889b1b896db5137280c624e590dc8
noelcv <79803356+noelcv@users.noreply.github.com>
2022-09-15 19:59:15 +0200

    security: custom security policies

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan"
 import helmet from 'helmet'
-import CONTENT_SECURITY_POLICY from './config/contentSecurityPolicy'
+import SECURITY_OPTIONS from "./config/helmet.config";
 const PORT = process.env.PORT || 3001;
 const allowedOrigins = [process.env.PRODUCTION_CLIENT_URL, process.env.DEV_CLIENT_URL]
 
@@ -23,24 +23,8 @@ app.use(cors({
   methods: ['GET'],
 }
 ));
-app.use(helmet())
-app.use(helmet(
-  { 
-    contentSecurityPolicy: CONTENT_SECURITY_POLICY, 
-    crossOriginEmbedderPolicy: true, 
-    crossOriginOpenerPolicy: { policy: "same-origin" }, 
-    crossOriginResourcePolicy: { policy: "same-site" },
-    dnsPrefetchControl: { allow: true },
-    expectCt: { enforce: true, maxAge: 30 },
-    frameguard: { action: "deny" },
-    hidePoweredBy: true,
-    hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
-    ieNoOpen: true,
-    noSniff: true,
-    permittedCrossDomainPolicies: { permittedPolicies: "none" },
-    referrerPolicy: { policy: "same-origin" },
-    xssFilter: true,
-  }))
+app.use(helmet(SECURITY_OPTIONS))
+app.disable('x-powered-by'); //Hide the information about the framework used and save bandwidth
 
 app.use(morgan("dev"));
 app.use(Express.json());
